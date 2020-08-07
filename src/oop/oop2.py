0ea5df3c7ac9b1e3b2eb2441e0bfc7a3dd80dab2
Poenomaly <djbenson741@gmail.com>
2020-08-07 10:08:10 -0600

    Completed MVP for all directories and completed stretch goals.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels=4):
+    def __init__(self, num_wheels=4):  # default num_wheels to 4
         self.num_wheels = num_wheels
 
     def drive(self):
@@ -19,11 +19,11 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 class Motorcycle(GroundVehicle):
-    def __init__(self, num_wheels=2):
-        super(Motorcycle, self).__init__(num_wheels)
+    def __init__(self, num_wheels=2):  # set new default num_wheels to 2
+        super(Motorcycle, self).__init__(num_wheels)  # super for parent class (GroundVehicle)
 
     def drive(self):
-        return "BRAAAP!!"
+        return "BRAAAP!!"  # Override 'drive' method in the GroundVehicle class
 
 vehicles = [
     GroundVehicle(),
