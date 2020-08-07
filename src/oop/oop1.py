84879a9eeb56f3e3bd3e3a655021a4d2e9e5f63f
Poenomaly <djbenson741@gmail.com>
2020-08-07 09:31:10 -0600

    Completed MVP for oop and comp directories. Tests ran successfully.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,52 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:  # This is the base class for all other classes
+
+    def __init__(self):
+        pass
+
+
+class FlightVehicle(Vehicle):  # This is the base class for Starship and Airplane classes
+
+    def __init__(self):
+
+        super(FlightVehicle, self).__init__()
+        pass
+
+
+class GroundVehicle(Vehicle):  # This is the base class for Car and Motorcycle classes
+
+    def __init__(self):
+        super(GroundVehicle, self).__init__()
+        pass
+
+
+class Starship(FlightVehicle):
+
+    def __init__(self):
+        super(Starship, self).__init__()
+        pass
+
+
+class Airplane(FlightVehicle):
+
+    def __init__(self):
+        super(Airplane, self).__init__()
+        pass
+
+
+class Car(GroundVehicle):
+
+    def __init__(self):
+        super(Car, self).__init__()
+        pass
+
+
+class Motorcycle(GroundVehicle):
+
+    def __init__(self):
+        super(Motorcycle, self).__init__()
+        pass
